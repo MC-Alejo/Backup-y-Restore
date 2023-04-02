8c35a55ea7a6037e273be02e86e7b72ea8b70c9f
Alejo <113360619+MC-Alejo@users.noreply.github.com>
2023-04-02 18:54:58 -0300

    Delete backups.bat

--- scripts/backups.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-SET PG_HOST=localhost
-SET PG_PORT=5432
-SET PG_DATABASE=CALP
-SET PG_USER=postgres
-SET PGPASSWORD=usuario
-SET PG_BIN="C:\Program Files\PostgreSQL\15\bin\pg_dump.exe"
-SET PG_FILENAME="%cd%\backups\BACKUP_2023-02-26_20-53-32.272607.backup"
-%PG_BIN% -h %PG_HOST% -p %PG_PORT% -U %PG_USER% --format custom -b -f %PG_FILENAME% %PG_DATABASE%
